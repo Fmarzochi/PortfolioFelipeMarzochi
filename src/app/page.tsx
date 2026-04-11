964d8c54b54389b429dc27235c856356d9900515
Felipe Marzochi <fmarzochi@gmail.com>
2026-04-11 01:21:57 -0300

    Eu configurei a pagina inicial do Next.js para rodar o WebOS

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { WebOS } from '../components/os/WebOS';
+
+export default function Home() {
+  return (
+    <main className="h-screen w-screen overflow-hidden bg-black text-white">
+      <WebOS />
+    </main>
+  );
+}
