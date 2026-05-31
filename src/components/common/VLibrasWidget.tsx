443ff960f642ccf73fc16cf2e204334aba1ef7b3
Felipe Marzochi <fmarzochi@gmail.com>
2026-05-31 00:32:30 -0300

    fix(a11y): instala vlibras-nextjs e simplifica VLibrasWidget

--- src/components/common/VLibrasWidget.tsx
+++ src/components/common/VLibrasWidget.tsx
@@ -1,31 +1,7 @@
 'use client';
 
-import { useEffect } from 'react';
-import Script from 'next/script';
+import VLibras from 'vlibras-nextjs';
 
 export const VLibrasWidget = () => {
-  const vwProps = { vw: 'true' } as Record<string, string>;
-  const accessButtonProps = { 'vw-access-button': 'true' } as Record<string, string>;
-  const wrapperProps = { 'vw-plugin-wrapper': 'true' } as Record<string, string>;
-
-  return (
-    <>
-      <div {...vwProps} className="enabled">
-        <div {...accessButtonProps} className="active"></div>
-        <div {...wrapperProps}>
-          <div className="vw-plugin-top-wrapper"></div>
-        </div>
-      </div>
-      <Script
-        id="vlibras-plugin"
-        src="https://vlibras.gov.br/app/vlibras-plugin.js"
-        strategy="afterInteractive"
-        onLoad={() => {
-          if ((window as any).VLibras) {
-            new (window as any).VLibras.Widget('https://vlibras.gov.br/app');
-          }
-        }}
-      />
-    </>
-  );
+  return <VLibras forceOnload={true} />;
 };
